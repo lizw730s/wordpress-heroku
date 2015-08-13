251e29379435c3605a32fcd476619176d0dca60b
yune <zyune@qq.com>
2015-08-13 23:30:02 +0800

    upload images to S3

--- wp-content/themes/twentyeleven/index.php
+++ wp-content/themes/twentyeleven/index.php
@@ -22,11 +22,12 @@ get_header(); ?>
 				<?php twentyeleven_content_nav( 'nav-above' ); ?>
 
 				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+				
+				<?php $i = 1; while (have_posts() && $i < 4) : the_post(); ?>
 
 					<?php get_template_part( 'content', get_post_format() ); ?>
 
-				<?php endwhile; ?>
+				<?php $i++; endwhile; ?>
 
 				<?php twentyeleven_content_nav( 'nav-below' ); ?>
 
